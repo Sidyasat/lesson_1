4b26904948bf84bf82045f128cd44fa45c32a038
Sidyasbsp <sidya@narod.ru>
2019-09-25 21:41:14 +0500

    Добавил проверки на введенные данные по бюджету на месяц, обязательную сумму расходов и округление ежедневного бюджета до 2 цифр после запятой. Также создана функция, в которой идет запрос к пользователю о сумме накоплений и проценте дохода

--- js/script.js
+++ js/script.js
@@ -1,24 +1,28 @@
 'use strict';
 
-let budget = prompt(`Ваш бюджет на месяц?`, `30000`);
-let data = prompt(`Введите дату в формате YYYY-MM-DD`, `2019-05-17`);
-
-let expenses = {
-    expenseItem: prompt(`Введите обязательную статью расходов в 
-                        этом месяце`, ``),
-    costExpense: prompt(`Во сколько обойдется`, ``)
-};
+let money = +prompt(`Ваш бюджет на месяц?`, `30000`);
+let time = prompt(`Введите дату в формате YYYY-MM-DD`, `2019-05-17`);
 
 let appData = {
-    budgetPerson: budget,
-    timeData: data,
-
-    expensesPerson: expenses,
-    optionalExpenses: null,
-    income: null,
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
     savings: false
 };
 
-let dailyBudget = (appData.budgetPerson - expenses.costExpense) / 30;
+for(let i = 0; i < 2; i++) {
+    let a = prompt(`Введите обязательную статью расходов в этом месяце`, ``);
+    let b = prompt(`Во сколько обойдется?`, ``);
 
-alert(dailyBudget);
+    if ((typeof(a)) === `string` && (typeof(a)) != null && (typeof(b)) != null && a != `` && b != `` && a.length < 50) {
+        appData.expenses[a] = b;
+    } else {
+        i--;
+    }
+}
+
+appData.dailyBudget = appData.budget/ 30;
+
+alert(`Ежедневный бюджет: ` + appData.dailyBudget);
