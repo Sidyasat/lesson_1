9527d4ab032bdcb9794996de5825372db5c386da
Sidyasbsp <sidya@narod.ru>
2019-10-08 20:45:35 +0500

    Добавил вопрос про дополнительные расходы

--- js/script.js
+++ js/script.js
@@ -20,45 +20,47 @@ let appData = {
     expenses: {},
     optionalExpenses: {},
     income: [],
-    savings: false
-};
-
-function chooseExpenses() {
-    for(let i = 0; i < 2; i++) {
-        let a = prompt(`Введите обязательную статью расходов в этом месяце`, ``);
-        let b = prompt(`Во сколько обойдется?`, ``);
-    
-        if ((typeof(a)) === `string` && (typeof(a)) != null && (typeof(b)) != null && a != `` && b != `` && a.length < 50) {
-            appData.expenses[a] = b;
-        } else {
-            i--;
+    savings: false,
+    chooseExpenses: function() {
+        for(let i = 0; i < 2; i++) {
+            let a = prompt(`Введите обязательную статью расходов в этом месяце`, ``);
+            let b = prompt(`Во сколько обойдется?`, ``);
+        
+            if ((typeof(a)) === `string` && (typeof(a)) != null && (typeof(b)) != null && a != `` && b != `` && a.length < 50) {
+                appData.expenses[a] = b;
+            } else {
+                i--;
+            }
         }
-    }
-}
-
-chooseExpenses();
-
-function detectLevel() {
-    appData.dailyBudget = (appData.budget/ 30).toFixed(2);
-}
-
-function detectDayBudget() {
-    detectLevel();
+    },
+    detectDayBudget: function() {
+        appData.dailyBudget = (appData.budget/ 30).toFixed(2);
     
-    alert(`Ежедневный бюджет: ` + appData.dailyBudget);
-}
-
-detectDayBudget();
-
-function checkSavings() {
-    if (appData.savings == true) {
-        let save = +prompt(`Какова сумма накоплений?`, ``);
-        let percent = +prompt(`Под какой процент?`, ``);
-
-        appData.monthIncome = save / 100 / 12 * percent;
-
-        alert(`Доход в месяц с вашего депозита: ` + appData.monthIncome);
+        alert(`Ежедневный бюджет: ` + appData.dailyBudget);
+    },
+    checkSavings: function(){
+        if (appData.savings == true) {
+            let save = +prompt(`Какова сумма накоплений?`, ``);
+            let percent = +prompt(`Под какой процент?`, ``);
+    
+            appData.monthIncome = save / 100 / 12 * percent;
+    
+            alert(`Доход в месяц с вашего депозита: ` + appData.monthIncome);
+        }
+    },
+    chooseOptExpenses: function() {
+        for (let i = 0; i < 3; i++) {
+            let opt = prompt(`Введите статью необязательных расходов`, ``);
+            appData.optionalExpenses[i] = opt;
+        }
+    },
+    chooseIncome: function() {
+        let items = prompt(`Что принесет дополнительный доход? (Перечислите через запятую)`, ``);
+        this.income = items.split(`, `);
+        this.income.push(prompt(`Может что-то еще?`, ``));
+        this.income.sort();
+    },
+    detectLevel: function() {
+        
     }
-}
-
-checkSavings();
+};
